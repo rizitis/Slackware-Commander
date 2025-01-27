7899ce83166045f1cc841a54d0b143708f4d4d1f
Anagnostakis Ioannis <elahistos@yahoo.gr>
2025-01-27 22:33:27 +0200

    update
    Signed-off-by: Anagnostakis Ioannis <elahistos@yahoo.gr>

--- scmd.SlackBuild/doinst.sh
+++ scmd.SlackBuild/doinst.sh
@@ -1,38 +1,23 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
+update_config() {
+    config_file="$1"
+    new_config_file="$2"
+
+    if [ ! -f "$config_file" ]; then
+        mv "$new_config_file" "$config_file"
+    fi
+
+    if diff -u "$config_file" "$new_config_file" > /dev/null; then
+        rm "$new_config_file"
+    else
+        cp "$config_file" "${config_file}.orig"
+        echo "Attention: ${new_config_file} detected..."
+    fi
 }
 
-preserve_perms() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ -e $OLD ]; then
-    cp -a $OLD ${NEW}.incoming
-    cat $NEW > ${NEW}.incoming
-    mv ${NEW}.incoming $NEW
-  fi
-  config $NEW
-}
+update_config "etc/inxifetch/inxifetch.conf" "etc/inxifetch/inxifetch.conf.new"
+update_config "etc/captain-slack/cptn-main.ini" "etc/captain-slack/cptn-main.ini.new"
 
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
 
-schema_install blah.schemas
-preserve_perms etc/rc.d/rc.INIT.new
-config etc/configfile.new
 
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
@@ -42,7 +27,7 @@ if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
-# If other icon themes are installed, then add to/modify this as needed
+
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
@@ -54,11 +39,3 @@ if [ -e usr/share/glib-2.0/schemas ]; then
     /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
   fi
 fi
-
-# If needed -- be sure to sed @LIBDIR@ inside the build script
-chroot . /usr/bin/gio-querymodules @LIBDIR@/gio/modules/ 1> /dev/null 2> /dev/null
-
-if [ -x /usr/bin/install-info ]; then
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/blah.gz 2> /dev/null
-fi
-
