3beb1888a7c5bd22f7b171c9313cc42478b4d79a
Anagnostakis Ioannis <elahistos@yahoo.gr>
2025-04-13 21:56:00 +0300

    update main gui add option show/hide
    Signed-off-by: Anagnostakis Ioannis <elahistos@yahoo.gr>

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -20,6 +20,7 @@ public:
 
         // Title Section
         QLabel* titleLabel = new QLabel("<span style='font-family: purisa; font-weight: bold; font-size: large;'>SYSTEM UPDATE</span>");
+        titleLabel->setTextFormat(Qt::RichText);
         mainLayout->addWidget(titleLabel);
 
         // Slackpkg Package Management Buttons
@@ -39,13 +40,18 @@ public:
         // Slackpkg+ Configuration
         mainLayout->addWidget(createSlackpkgButton("whitelist", "nano /etc/slackpkg/whitelist"));
 
-        // Package Command Section (slackpkg build, install, reinstall, etc.)
-        QVBoxLayout* packageLayout = new QVBoxLayout();
+        // Collapsible Section Toggle
+        QPushButton* toggleButton = new QPushButton("▶ SHOW HIDDEN TOOLS");
+        mainLayout->addWidget(toggleButton);
+
+        // Collapsible Widget for Package Section
+        QWidget* packageWidget = new QWidget();
+        QVBoxLayout* packageLayout = new QVBoxLayout(packageWidget);
+
         QLineEdit* packageEntry = new QLineEdit();
         packageLayout->addWidget(new QLabel("Package:"));
         packageLayout->addWidget(packageEntry);
 
-        // Package management buttons
         packageLayout->addWidget(createPackageCommandButton("slackpkg_build", packageEntry));
         packageLayout->addWidget(createPackageCommandButton("slackpkg install", packageEntry));
         packageLayout->addWidget(createPackageCommandButton("slackpkg reinstall", packageEntry));
@@ -58,14 +64,21 @@ public:
         packageLayout->addWidget(createPackageCommandButton("--version", packageEntry));
         packageLayout->addWidget(createPackageCommandButton("man", packageEntry));
 
-        mainLayout->addLayout(packageLayout);
+        packageWidget->setVisible(false);  // Initially hidden
+        mainLayout->addWidget(packageWidget);
+
+        // Toggle logic with auto-resize
+        connect(toggleButton, &QPushButton::clicked, [this, toggleButton, packageWidget]() {
+            bool isVisible = packageWidget->isVisible();
+            packageWidget->setVisible(!isVisible);
+            toggleButton->setText(isVisible ? "▶ Show Hidden Tools" : "▼ Hide Again Tools");
+            this->adjustSize(); // Auto-resize the window
+        });
 
         // More Tools Button
         QPushButton* moreToolsButton = new QPushButton("MORE TOOLS");
         connect(moreToolsButton, &QPushButton::clicked, this, &scmd::openMoreTools);
         mainLayout->addWidget(moreToolsButton);
-
-        setLayout(mainLayout);
 
         // Slackpkg+ setup Button
         QPushButton* slackpkgplussButton = new QPushButton("slackpkg+ SetUp");
@@ -80,7 +93,6 @@ private:
         QPushButton* button = new QPushButton(label);
         connect(button, &QPushButton::clicked, [action]() {
             qDebug() << "Executing as root: " << action;
-            // Run the command as root using su -c
             QProcess::startDetached("/usr/bin/konsole", QStringList() << "--hold" << "-e" << "su" << "-c" << action);
         });
         return button;
@@ -91,26 +103,25 @@ private:
         connect(button, &QPushButton::clicked, [packageEntry, label]() {
             QString command;
 
-            // Special handling for --help or --version commands
-            if (label.startsWith("--")) {  // This handles --help or --version
+            if (label.startsWith("--")) {
                 command = packageEntry->text() + " " + label;
             } else {
                 command = label + " " + packageEntry->text();
             }
 
             qDebug() << "Executing as root: " << command;
-            // Run the command as root using su -c
             QProcess::startDetached("/usr/bin/konsole", QStringList() << "--hold" << "-e" << "su" << "-c" << command);
         });
         return button;
     }
 
     void openMoreTools() {
-    QProcess::startDetached("/usr/local/sbin/scmd2");
-}
+        QProcess::startDetached("/usr/local/sbin/scmd2");
+    }
+
     void openslackpkgpluss() {
-    QProcess::startDetached("/usr/local/sbin/scmd3");
-}
+        QProcess::startDetached("/usr/local/sbin/scmd3");
+    }
 };
 
 int main(int argc, char *argv[]) {
@@ -118,6 +129,7 @@ int main(int argc, char *argv[]) {
 
     scmd window;
     window.show();
+    window.adjustSize(); // Ensure initial sizing is tight
 
     return app.exec();
 }
