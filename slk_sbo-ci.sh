6dee3a3b77f4e1e9c9535079c4e9fccb597f5837
Anagnostakis Ioannis <elahistos@yahoo.gr>
2026-05-23 00:45:04 +0300

    WIP: some more fixes...
    Signed-off-by: Anagnostakis Ioannis <elahistos@yahoo.gr>

--- slk_sbo-ci.sh
+++ slk_sbo-ci.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-SBSCRIPT="$(find "${BUILDDIR}" -maxdepth 1 -name '*.SlackBuild' | head -1)"
+SBSCRIPT="$(find "${BUILDDIR:-.}" -maxdepth 1 -name '*.SlackBuild' | head -1)"
 JUST_NAME=$(basename "$SBSCRIPT" .SlackBuild)
-
 DEPS_FILE="$JUST_NAME".dep
-
 sbo_txt_url="https://slackbuilds.org/slackbuilds/15.0/SLACKBUILDS.TXT"
 sbo_txt_cache="/tmp/SLACKBUILDS.TXT"
 declare -A visited
 deplist=()
-
 
 fetch_sbo_txt() {
   if [[ ! -f "$sbo_txt_cache" ]]; then
@@ -19,33 +16,29 @@ fetch_sbo_txt() {
 }
 
 get_location() {
-  local name="$JUST_NAME"
-  grep -A5 "^SLACKBUILD NAME: $JUST_NAME$" "$sbo_txt_cache" \
+  local name="$1"
+  grep -A5 "^SLACKBUILD NAME: ${name}$" "$sbo_txt_cache" \
     | grep "^SLACKBUILD LOCATION:" | head -1 \
     | awk '{print $3}' | sed 's|^\./||'
 }
 
 get_requires() {
-  local name="$JUST_NAME"
-  grep -A10 "^SLACKBUILD NAME: $JUST_NAME$" "$sbo_txt_cache" \
+  local name="$1"
+  grep -A10 "^SLACKBUILD NAME: ${name}$" "$sbo_txt_cache" \
     | grep "^SLACKBUILD REQUIRES:" | head -1 \
     | sed 's/^SLACKBUILD REQUIRES://' | xargs
 }
 
 resolve_deps() {
-  local name="$JUST_NAME"
-
-  [[ "${visited[$JUST_NAME]+_}" ]] && return
-  visited[$JUST_NAME]=1
-
+  local name="$1"
+  [[ "${visited[$name]+_}" ]] && return
+  visited[$name]=1
   local location
-  location=$(get_location "$JUST_NAME")
+  location=$(get_location "$name")
   [[ -z "$location" ]] && return
-
   local requires
-  requires=$(get_requires "$JUST_NAME")
+  requires=$(get_requires "$name")
   [[ -z "$requires" ]] && return
-
   for dep in $requires; do
     [[ "$dep" == "%README%" ]] && continue
     resolve_deps "$dep"
@@ -54,35 +47,31 @@ resolve_deps() {
 }
 
 # --- main ---
-
 if [[ -z "$JUST_NAME" ]]; then
   _err "Ops $0 was not set properly JUST_NAME"
   exit 1
 fi
 
 if [ -f "$DEPS_FILE" ]; then
-    _log "deps file found for $JUST_NAME"
+  _log "deps file found for $JUST_NAME"
 else
-fetch_sbo_txt
+  fetch_sbo_txt
+  _log "Resolving dependencies for $JUST_NAME..."
+  resolve_deps "$JUST_NAME"
 
-_log "Resolving dependencies for $JUST_NAME..."
+  seen=()
+  declare -A seen_map
+  for dep in "${deplist[@]}"; do
+    if [[ ! "${seen_map[$dep]+_}" ]]; then
+      seen_map[$dep]=1
+      seen+=("$dep")
+    fi
+  done
 
-resolve_deps "$JUST_NAME"
-
-# deduplicate deps in order.
-seen=()
-declare -A seen_map
-for dep in "${deplist[@]}"; do
-  if [[ ! "${seen_map[$dep]+_}" ]]; then
-    seen_map[$dep]=1
-    seen+=("$dep")
+  if [[ ${#seen[@]} -eq 0 ]]; then
+    _log "No dependencies found."
+  else
+    printf '%s\n' "${seen[@]}" > "$DEPS_FILE"
+    _log "Done: $DEPS_FILE (${#seen[@]} packages)"
   fi
-done
-
-if [[ ${#seen[@]} -eq 0 ]]; then
-  _log "No dependencies found."
-else
-  printf '%s\n' "${seen[@]}" > "$JUST_NAME.dep"
-  _log "Done: $JUST_NAME.dep (${#seen[@]} packages)"
-fi
 fi
--- slk_sbo-pkg.sh
+++ slk_sbo-pkg.sh
@@ -1,63 +1,19 @@
 #!/bin/bash
 
+sbo_txt="/tmp/SLACKBUILDS.TXT"
 
 SBSCRIPT="$(find "${BUILDDIR}" -maxdepth 1 -name '*.SlackBuild' | head -1)"
 JUST_NAME=$(basename "$SBSCRIPT" .SlackBuild)
 DEPS_FILE="$JUST_NAME".dep
 
-if [ ! -f "$DEPS_FILE" ]; then
-    _log "no deps found for $JUST_NAME"
-else
-   while IFS= read -r dep_name; do
-        [ -z "$dep_name" ] && continue
-
-        _log "Looking up '${dep_name}' in SBo..."
-   location=$(curl -sL "$sbo_txt" | grep -A5 "^SLACKBUILD NAME: ${dep_name}$" | grep "^SLACKBUILD LOCATION:" | head -1 | awk '{print $3}' | sed 's|^\./||')
-
-     if [[ -z "$location" ]]; then
-        _err " '${dep_name}' not found in SLACKBUILDS.TXT"
-        _slk_detect_version
-        if [[ "$SLK_VERSION" == "current" ]]; then
-             _log "Maybe '${dep_name}' allready in current?"
-             _log "In this case should be mentioned on build_deps: in .yml file"
-             exit 1
-        fi
-
-     fi
-
-_log "Found: ${location}"
-
-tmp=$(mktemp -d)
-
-      git clone --depth=1 --filter=blob:none --sparse \
-          https://github.com/Ponce/slackbuilds.git -b current "$tmp/repo" -q
-
-        cd "$tmp/repo"
-        git sparse-checkout set "$location"
-
-        tar czf "${OLDPWD}/${dep_name}.tar.gz" -C "$tmp/repo/$location/.." "$dep_name"
-        cd "$OLDPWD"
-        rm -rf "$tmp"
-
-        tar xvf ${dep_name}.tar.gz
-        pushd ${dep_name} || exit 1
-        unset PKG OUTPUT
-        source ${dep_name}.info
-        chmod +x ${dep_name}.SlackBuild
-        bash ${dep_name}.SlackBuild
-        _log "${dep_name} Build succeeded."
-        _log "Installing ${dep_name}..."
-        installpkg --terse /tmp/$PRGNAM-$VERSION-*.tgz
-
-        slk_sbo_check_version() {
+slk_sbo_check_version() {
+    local dep_name="$1"
     local SBo_NAME="$dep_name"-"$VERSION"
     local SBo_EXPECTED="$PRGNAM-$VERSION"
-
     local SBo_INSTALLED
     SBo_INSTALLED="$(ls /var/log/packages/${SBo_NAME}-* 2>/dev/null \
-                  | sed "s|.*/${NAME}-||;s|-[^-]*-[^-]*\$||" \
+                  | sed "s|.*/${SBo_NAME}-||;s|-[^-]*-[^-]*\$||" \
                   | tail -1)"
-
     if [[ "${SBo_INSTALLED}" == "${SBo_EXPECTED}" ]]; then
         _log "Version check passed: ${dep_name} ${SBo_INSTALLED}"
     else
@@ -65,15 +21,57 @@ tmp=$(mktemp -d)
     fi
 }
 
+if [ ! -f "$DEPS_FILE" ]; then
+    _log "no deps found for $JUST_NAME"
+else
+    while IFS= read -r dep_name; do
+        [ -z "$dep_name" ] && continue
+        _log "Looking up '${dep_name}' in SBo..."
+        location=$(grep -A5 "^SLACKBUILD NAME: ${dep_name}$" "$sbo_txt" \
+            | grep "^SLACKBUILD LOCATION:" | head -1 \
+            | awk '{print $3}' | sed 's|^\./||')
 
-        slk_sbo_check_version
-        # clean up
-        rm /tmp/$PRGNAM-$VERSION-*.tgz
+        if [[ -z "$location" ]]; then
+            _err "'${dep_name}' not found in SLACKBUILDS.TXT"
+            _slk_detect_version
+            if [[ "$SLK_VERSION" == "current" ]]; then
+                _log "Maybe '${dep_name}' already in current?"
+                _log "In this case should be mentioned on build_deps: in .yml file"
+                exit 1
+            fi
+        fi
+
+        _log "Found: ${location}"
+
+        tmp=$(mktemp -d)
+        git clone --depth=1 --filter=blob:none --sparse \
+            https://github.com/Ponce/slackbuilds.git -b current "$tmp/repo" -q
+        cd "$tmp/repo"
+        git sparse-checkout set "$location"
+        tar czf "${OLDPWD}/${dep_name}.tar.gz" -C "$tmp/repo/$location/.." "$dep_name"
+        cd "$OLDPWD"
+        rm -rf "$tmp"
+
+        tar xf "${dep_name}.tar.gz"
+        pushd "${dep_name}" || exit 1
+
+        unset PKG OUTPUT
+        source "${dep_name}.info"
+        chmod +x "${dep_name}.SlackBuild"
+        bash "${dep_name}.SlackBuild"
+        _log "${dep_name} Build succeeded."
+
+        _log "Installing ${dep_name}..."
+        installpkg --terse /tmp/$PRGNAM-$VERSION-*.t?z
+
+        slk_sbo_check_version "$dep_name"
+
+        rm /tmp/$PRGNAM-$VERSION-*.t?z
+
         info_vars=$(grep -v '^#' "${dep_name}.info" | cut -d= -f1)
         popd || exit 1
         unset $info_vars
 
         _log "All Done: ${dep_name}"
-
     done < "$DEPS_FILE"
 fi
